6153a003fb695c852787d06920bdc7d1005a7f70
ArtemJDS <vasilev.artem.8.7.03@gmail.com>
2022-10-12 22:17:05 +0300

    difference input is added for both dimensions (left-right, up-down) (at last)

--- run.pyx
+++ run.pyx
@@ -216,23 +216,25 @@ cdef class Enviroment:
 
         elif a == 4:
                 if self.x + 1 < self.enviroment.shape[1] and self.x + 1 >= 0 :
-                    return self.enviroment[self.y][self.x + 1][0] - self.enviroment[self.y][self.x]
+                    return self.enviroment[self.y][self.x + 1][0] - self.enviroment[self.y][self.x][0]
                 else:
                     return 0.
-        elif a == 5:
 
+        elif a == 5:
                 if self.x - 1 < self.enviroment.shape[1] and self.x -1 >= 0:
-                    return self.enviroment[self.y][self.x - 1][0] - self.enviroment[self.y][self.x]
+                    return self.enviroment[self.y][self.x - 1][0] - self.enviroment[self.y][self.x][0]
                 else:
                     return 0.
+
         elif a == 6:
                 if self.y - 1 >= 0 and self.y - 1 < self.enviroment.shape[0]:
-                    return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x]
+                    return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x][0]
                 else:
                     return 0.
+
         elif a == 7:
                 if self.y - 1 >= 0 and self.y - 1 < self.enviroment.shape[0]:
-                    return self.enviroment[self.y - 1][self.x][0]
+                    return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x][0]
                 else:
                     return 0.
 
@@ -285,26 +287,25 @@ cdef class Enviroment:
 
     cdef double df_input_1(self) nogil:
         if self.x + 1 < self.enviroment.shape[1] and self.x + 1 >= 0 :
-            return self.enviroment[self.y][self.x + 1][0] - self.enviroment[self.y][self.x]
+            return self.enviroment[self.y][self.x + 1][0] - self.enviroment[self.y][self.x][0]
         else:
             return 0.
 
     cdef double df_input_2(self) nogil:
         if self.x - 1 < self.enviroment.shape[1] and self.x -1 >= 0:
-            return self.enviroment[self.y][self.x - 1][0] - self.enviroment[self.y][self.x]
+            return self.enviroment[self.y][self.x - 1][0] - self.enviroment[self.y][self.x][0]
         else:
             return 0.
 
     cdef double df_input_3(self) nogil:
         if self.y - 1 >= 0 and self.y - 1 < self.enviroment.shape[0]:
-            return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x]
+            return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x][0]
         else:
             return 0.
 
-
     cdef double df_input_4(self) nogil:
         if self.y - 1 >= 0 and self.y - 1 < self.enviroment.shape[0]:
-            return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x]
+            return self.enviroment[self.y - 1][self.x][0] - self.enviroment[self.y][self.x][0]
         else:
             return 0.
 
